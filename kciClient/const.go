74aa8c72f670eb7718f4f432564773b3073c1c25
leiiwang <u2takey@gmail.com>
2016-11-17 19:22:57 +0800

    del key in yml

--- kciClient/const.go
+++ kciClient/const.go
@@ -4,5 +4,5 @@ const kciUrl = "https://kci.qiniu.com"
 
 // your ak sk from portal
 // !!! you should auth with portal first !!!
-const ak = "OyUK0elDG7mdbKi15_9VpA-ly-pWmCu51SnYLS-K"
-const sk = "cng3Z3oSTFvIQu8j41pXbrE1dYx9UKXtjvNfFOTS"
+const ak = ""
+const sk = ""
